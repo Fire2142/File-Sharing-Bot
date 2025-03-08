a89fdaf22de5fb99ca1e565844df045d73246683
Mr Ghost <s.rytgam.ing89@gmail.com>
2025-03-08 22:15:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
+START_MSG = os.environ.get("START_MESSAGE", "Hello {username}✨\n\nI am a Permanent file store bot and users can access stored messages by using a shareable link given by me.\n\nTo know more click help button.")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
